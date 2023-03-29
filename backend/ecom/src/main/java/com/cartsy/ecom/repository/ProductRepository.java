1012c7efe3948997ab62996eea41a3f9909bf168
Akhileshsigma <129247527+Akhileshsigma@users.noreply.github.com>
2023-03-29 19:43:34 +0530

    Revert "Develop"

--- backend/ecom/src/main/java/com/cartsy/ecom/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cartsy.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cartsy.ecom.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long>
-{}
