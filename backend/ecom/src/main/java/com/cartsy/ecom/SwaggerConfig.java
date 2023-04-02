f75e19d62213eaa509b062db136442f199c3867a
Devaraj33 <Devarajissur33@gmai.com>
2023-04-02 15:58:35 +0530

    Changing custom name in swagger

--- backend/ecom/src/main/java/com/cartsy/ecom/SwaggerConfig.java
+++ backend/ecom/src/main/java/com/cartsy/ecom/SwaggerConfig.java
@@ -1,10 +1,13 @@
 package com.cartsy.ecom;
 
+import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.Contact;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,16 +15,27 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-	
-	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)
-          .host("http://localhost:8080")
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .host("http://localhost:8080")
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Ecommerce_Project",
+                "API for managing products",
+                "1.0.0",
+                "Terms of service",
+                new Contact("John Doe", "www.example.com", "johndoe@example.com"),
+                "License of API", "API license URL",
+                Collections.emptyList());
+    }
+
 }
-
-
