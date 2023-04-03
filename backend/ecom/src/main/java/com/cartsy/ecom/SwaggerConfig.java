14ef2537b71dd479bb81444337a9ef57d1446e9a
Shridhar <shridharsiri04@gmail.com>
2023-04-02 21:01:46 -0700

    commited swaggerconfig.java

--- backend/ecom/src/main/java/com/cartsy/ecom/SwaggerConfig.java
+++ backend/ecom/src/main/java/com/cartsy/ecom/SwaggerConfig.java
@@ -7,37 +7,36 @@ import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-<<<<<<< HEAD
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
+
 public class SwaggerConfig {                                    
     @Bean
     public Docket api() { 
         return new Docket(DocumentationType.SWAGGER_2)  
-        		.host("https://localhost:8080")
-=======
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+        		.host("https://localhost:8080");
 
-@Configuration
-@EnableSwagger2
-public class SwaggerConfig {
+    }
+
+
 	
 	@Bean
-    public Docket api() { 
+    public Docket api1() { 
         return new Docket(DocumentationType.SWAGGER_2)
->>>>>>> b9364eea35f391eae1017b2825d97f6059907404
+
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
           .build();                                           
     }
-<<<<<<< HEAD
+
 }
 
 
 
-=======
+
 
 	
-}
->>>>>>> b9364eea35f391eae1017b2825d97f6059907404
+
+
