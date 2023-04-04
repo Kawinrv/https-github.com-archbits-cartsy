56fec2e1a6b50f1b3cd4d4a03f155ec1e3e73be2
archbits <umar.ashraf.2311@gmail.com>
2023-04-05 02:27:12 +0530

    meaningful apis

--- backend/ecom/src/main/java/com/cartsy/ecom/repository/ProductCategoryRepository.java
+++ backend/ecom/src/main/java/com/cartsy/ecom/repository/ProductCategoryRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cartsy.ecom.model.ProductCategory;
 
 
-public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long>{
+public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Integer>{
 
 }
