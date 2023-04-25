929c2d6528f32e1c98e86c0cf65b0d6d83c247f4
Devaraj33 <Devarajissur33@gmai.com>
2023-04-25 22:22:48 +0530

    New Changes

---
+++ backend/ecom/src/main/java/com/cartsy/ecom/repository/OrderRepository.java
@@ -0,0 +1,28 @@
+package com.cartsy.ecom.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.cartsy.ecom.api.v1.model.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Integer>{
+
+
+
+	@Query("SELECT o FROM Order o WHERE o.buyerId = :id")
+	List<Order> byBuyer(@Param("id") Integer id);
+
+	@Query("SELECT o FROM Order o WHERE o.sellerId = :id")
+	List<Order> bySeller(@Param("id") Integer id);
+
+	@Query("SELECT o FROM Order o WHERE o.dateOfOrder >= :creationDate")
+	List<Order> filterByCreationDate(@Param("creationDate") Date creationDate);
+
+
+
+
+}
