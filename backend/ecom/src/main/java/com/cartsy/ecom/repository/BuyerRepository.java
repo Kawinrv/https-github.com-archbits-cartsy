fa7d077c49a709adbcf8e06443c4b6c15c626bee
Devaraj33 <Devarajissur33@gmai.com>
2023-04-25 21:51:19 +0530

    Deleted

--- backend/ecom/src/main/java/com/cartsy/ecom/repository/BuyerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cartsy.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cartsy.ecom.model.Buyer;
-
-public interface BuyerRepository extends JpaRepository<Buyer, Long>{
-
-}
