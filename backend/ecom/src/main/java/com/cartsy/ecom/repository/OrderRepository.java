e0caacc011442b07220987366b5dc031ebb9df56
Devaraj33 <Devarajissur33@gmai.com>
2023-04-25 22:08:15 +0530

    Delete

--- backend/ecom/src/main/java/com/cartsy/ecom/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cartsy.ecom.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.cartsy.ecom.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-
-}
